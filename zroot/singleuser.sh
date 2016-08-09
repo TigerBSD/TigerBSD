27bebdd36ca8fff549f32dfbae4582e380799e7e
Erik Nordstrøm <erik@nordstroem.no>
2016-08-09 09:52:10 +0200

    Add comment to singleuser.sh.

--- zroot/singleuser.sh
+++ zroot/singleuser.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+#
+# Run this script after booting into single user mode.
+#
+# These commands were taken from section 23.6 Rebuilding World
+# of the FreeBSD Handbook [1], but are useful in general
+# when in single user mode.
+#
+# [1]: https://www.freebsd.org/doc/handbook/makeworld.html#idp77127408
+#
+
 mount -u /
 mount -a -t ufs
 swapon -a
